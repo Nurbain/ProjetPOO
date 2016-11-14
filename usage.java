5f927e3bddec843ccc736c5fc70ffba73b3bdddb
mattleon <matthieu.leon@etu.unistra.fr>
2016-11-14 20:29:10 +0100

    Add files via upload

---
+++ usage.java
@@ -0,0 +1,14 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Stery on 14/11/2016.
+ */
+
+public interface usage {
+
+    double pixelX(double x[][], double y[][], BufferedImage img, int i, int j);
+
+    double pixelY(double x[][], double y[][], BufferedImage img, int i, int j);
+
+    String new_name(String name_file, String filtre);
+}
